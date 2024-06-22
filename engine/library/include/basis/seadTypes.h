f46d579c0750de4ddf6aa9ee6b5ce28b3bb1bf10
splatoon1enjoyer <administrator@sdlfoundation.com>
2024-06-22 23:43:13 +0300

    Fix types.h include error

--- engine/library/include/basis/seadTypes.h
+++ engine/library/include/basis/seadTypes.h
@@ -1,6 +1,9 @@
 #ifndef SEAD_TYPES_H_
 #define SEAD_TYPES_H_
 
+#include <sdl-utils/Types.h>
+
+/*
 #ifdef cafe
 
 #include <cstddef>
@@ -30,13 +33,15 @@ typedef sintptr  intptr_t;
     #define TOKENPASTE(x, y) x ## y
     #define TOKENPASTE2(x, y) TOKENPASTE(x, y)
 
-    #define static_assert(condition, ...) typedef int TOKENPASTE2(static_assert_, __LINE__)[(condition) ? 1 : -1]
-#endif // static_assert
+    #define static_assert(condition, ...) typedef int
+TOKENPASTE2(static_assert_, __LINE__)[(condition) ? 1 : -1] #endif //
+static_assert
 
 #else
 
 #error "Unknown platform"
 
 #endif // cafe
+*/
 
 #endif // SEAD_TYPES_H_
